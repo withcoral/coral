da271da273bc322a2ada15450a8af712ea277a5b
Alberto Morabito <alberto@phoebe.ai>
2026-07-28 12:38:39 +0100

    fix(reef): export function explorer

--- apps/reef/app/components/functions/index.ts
+++ apps/reef/app/components/functions/index.ts
@@ -4,5 +4,6 @@ export {
   type FunctionDetailsProps,
   type FunctionDetailsResultColumn,
 } from './function-details'
+export { FunctionExplorer, type FunctionExplorerProps } from './function-explorer'
 export { FunctionList, type FunctionListItem, type FunctionListProps } from './function-list'
 export { FunctionSources, type FunctionSourcesProps } from './function-sources'
