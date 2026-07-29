3895a90c906610d89e231fec8430f10236706621
Alberto Morabito <alberto@phoebe.ai>
2026-07-29 14:55:44 +0100

    chore(reef): remove obsolete function list

--- apps/reef/app/components/functions/index.ts
+++ apps/reef/app/components/functions/index.ts
@@ -5,5 +5,4 @@ export {
   type FunctionDetailsResultColumn,
 } from './function-details'
 export { FunctionExplorer, type FunctionExplorerProps } from './function-explorer'
-export { FunctionList, type FunctionListItem, type FunctionListProps } from './function-list'
 export { FunctionSources, type FunctionSourcesProps } from './function-sources'
